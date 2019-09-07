5557e544693264a767a44391a8504fafa8cad1a9
Matheus <matheuscorreia559@gmail.com>
2019-09-07 10:34:01 -0400

    atualizando rota de usuarios

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,52 +1,59 @@
-const express = require('express')
+const express = require("express");
 
-const User = require('../models/User.js')
+const User = require("../models/User.js");
 
-const router = express.Router()
+const router = express.Router();
 
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
+const authMiddleware = require("../middleware/auth");
 
-function generateToken(params = {}){
-    return jwt.sign(params,process.env.secret,
-        {expiresIn:86400})
+const expiresIn = 10;
+
+function generateToken(params = {}) {
+  return jwt.sign(params, process.env.secret, { expiresIn });
 }
 
-router.post('/register', async (req, res)=>{
-    const { email, name } = req.body
+router.post("/register", async (req, res) => {
+  const { email, name } = req.body;
 
-    
-    try{
-        if(await User.findOne({email})){
-            return res.status(400).send({error:"User already exists"})
-        }    
-        console.log(req.body)
-        const user =  await User.create(req.body)
-        user.password = undefined
-        return res.status(200).send({user, token:generateToken({id:user._id})})
-    }catch(err){
-        return res.status(400).send({error:'Registration fail'})
+  try {
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ error: "User already exists" });
     }
-})
+    console.log(req.body);
+    const user = await User.create(req.body);
+    user.password = undefined;
+    return res
+      .status(200)
+      .send({ user, token: generateToken({ id: user._id }) });
+  } catch (err) {
+    return res.status(400).send({ error: "Registration fail" });
+  }
+});
 
-router.post('/authenticate', async (req, res) => {
-    const { email, password } = req.body
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email }).select('+password')
+  const user = await User.findOne({ email }).select("+password");
 
-    if(!user){
-        return res.status(400).send({erro:"User not found"})
+  if (!user) {
+    return res.status(400).send({ erro: "User not found" });
+  }
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res.status(400).send({ error: "invalid password" });
+  }
+  user.password = undefined;
 
-    }
-    if(!await bcrypt.compare(password, user.password)){
-        return res.status(400).send({error:"invalid password"})
-    }
-    user.password = undefined
- 
-    res.status(200).send({ user, token:generateToken({id:user._id}) })
-})
+  res
+    .status(200)
+    .send({ user, token: generateToken({ id: user._id }), expiresIn });
+});
 
+router.post("/verify", authMiddleware, async (req, res) => {
+  res.status(200).send();
+});
 
-module.exports = app => app.use('/auth',router)
+module.exports = app => app.use("/auth", router);
--- src/index.js
+++ src/index.js
@@ -22,49 +22,9 @@ require("./controllers/filePostController")(app);
 require("./controllers/onePagController")(app);
 require("./controllers/postController")(app);
 
-const porta = process.env.PORT || 3000;
+const porta = process.env.PORT || 3001;
 console.log("[D]Porta: " + porta);
 app.listen(porta);
 
 console.log("All routes:");
 console.log(all_routes(app));
-
-// function print(path, layer) {
-//   if (layer.route) {
-//     layer.route.stack.forEach(
-//       print.bind(null, path.concat(split(layer.route.path)))
-//     );
-//   } else if (layer.name === "router" && layer.handle.stack) {
-//     layer.handle.stack.forEach(
-//       print.bind(null, path.concat(split(layer.regexp)))
-//     );
-//   } else if (layer.method) {
-//     console.log(
-//       "%s /%s",
-//       layer.method.toUpperCase(),
-//       path
-//         .concat(split(layer.regexp))
-//         .filter(Boolean)
-//         .join("/")
-//     );
-//   }
-// }
-
-// function split(thing) {
-//   if (typeof thing === "string") {
-//     return thing.split("/");
-//   } else if (thing.fast_slash) {
-//     return "";
-//   } else {
-//     var match = thing
-//       .toString()
-//       .replace("\\/?", "")
-//       .replace("(?=\\/|$)", "$")
-//       .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//);
-//     return match
-//       ? match[1].replace(/\\(.)/g, "$1").split("/")
-//       : "<complex:" + thing.toString() + ">";
-//   }
-// }
-
-// app._router.stack.forEach(print.bind(null, []));
