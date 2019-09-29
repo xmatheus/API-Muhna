ed2910926fe0d913831d55de8e6032806e1aec51
Matheus <matheuscorreia559@gmail.com>
2019-09-28 20:26:56 -0400

    alterando o limit de upload apenas para teste de aplicacao

--- src/models/News/upload.js
+++ src/models/News/upload.js
@@ -40,7 +40,7 @@ const storage = new GridFsStorage({
 const upload = multer({
 	storage: storage,
 	limits: {
-		fileSize: 15 * 1024 * 1024
+		fileSize: 2 * 1024 * 1024
 	},
 	fileFilter: (req, file, cb) => {
 		const allowedMimes = [
